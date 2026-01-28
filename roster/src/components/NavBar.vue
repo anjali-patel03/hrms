8cf121733d4dd466c130a127949e262facca4525
Anjali Patel <anjali.patel@artemhealthtech.com>
2026-01-28 11:01:42 +0530

    branding: rebrand ERPNext to BMC HR with custom logos and navbar

--- roster/src/components/NavBar.vue
+++ roster/src/components/NavBar.vue
@@ -1,9 +1,13 @@
 <template>
 	<div class="h-12 bg-white border-b px-12 flex items-center">
 		<div class="flex items-center space-x-1.5">
+#			<a href="/app/hr" class="text-gray-600 hover:text-gray-700 flex items-center">
+#				<FrappeHRLogo class="h-6 w-6 mr-2.5" />
+#				BMC HR
+#			</a>
 			<a href="/app/hr" class="text-gray-600 hover:text-gray-700 flex items-center">
-				<FrappeHRLogo class="h-6 w-6 mr-2.5" />
-				Frappe HR
+				<img src="../assets/icons/bmc-logo.svg" class="h-6 w-6 mr-2.5" alt="logo" />
+				BMC HR
 			</a>
 			<FeatherIcon name="chevron-right" class="h-4 w-4" />
 			<span class="font-medium">Roster</span>
@@ -37,7 +41,7 @@
 
 <script setup lang="ts">
 import { FeatherIcon, Dropdown, Avatar, createResource } from "frappe-ui";
-import FrappeHRLogo from "../icons/FrappeHRLogo.vue";
+# import FrappeHRLogo from "../icons/FrappeHRLogo.svg";
 
 import { User } from "../views/Home.vue";
 import { goTo, raiseToast } from "../utils";
